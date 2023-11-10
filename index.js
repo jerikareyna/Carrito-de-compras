d19bf3dd3cd6cc11eb86ace13197789dc5c22cb5
jerikareyna <105334298+jerikareyna@users.noreply.github.com>
2023-11-10 09:45:51 -0500

    test duplicate productCar

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const minusButton = document.querySelector('.quantity__minus');
-    const plusButton = document.querySelector('.quantity__plus');
-    const inputField = document.querySelector('.quantity__input');
-  
-    minusButton.addEventListener('click', () => {
-      let currentValue = Number(inputField.value);
-      if (currentValue > 0) {
-        inputField.value = currentValue - 1;
-      }
-    });
-  
-    plusButton.addEventListener('click', () => {
-      let currentValue = Number(inputField.value);
-      inputField.value = currentValue + 1;
-    });
+  const minusButtons = document.querySelectorAll('.quantity__minus');
+  const plusButtons = document.querySelectorAll('.quantity__plus');
+
+  minusButtons.forEach((minusButton) => {
+      minusButton.addEventListener('click', () => {
+          const inputField = minusButton.parentElement.querySelector('.quantity__input');
+          let currentValue = Number(inputField.value);
+          if (currentValue > 0) {
+              inputField.value = currentValue - 1;
+          }
+      });
   });
+
+  plusButtons.forEach((plusButton) => {
+      plusButton.addEventListener('click', () => {
+          const inputField = plusButton.parentElement.querySelector('.quantity__input');
+          let currentValue = Number(inputField.value);
+          inputField.value = currentValue + 1;
+      });
+  });
+});
